3ce6edd7a93a1f48ed0cd7735d4adc2ff0326e7c
mikee385 <mikee385@gmail.com>
2020-01-17 16:17:42 -0600

    Fix extra parenthesis

--- drivers/person-status.groovy
+++ drivers/person-status.groovy
@@ -14,7 +14,7 @@
  *
  */
 metadata {
-    definition (name: "Person Status", namespace: "mikee385", author: "Michael Pierce", importUrl: "https://raw.githubusercontent.com/mikee385/hubitat-mikee385/master/drivers/person-status.groovy") {) {
+    definition (name: "Person Status", namespace: "mikee385", author: "Michael Pierce", importUrl: "https://raw.githubusercontent.com/mikee385/hubitat-mikee385/master/drivers/person-status.groovy") {
         capability "Actuator"
         capability "Presence Sensor"
 		capability "Sensor"
