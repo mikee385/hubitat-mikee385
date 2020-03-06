afe9b531fb12db54e1c2d488ef4e4284245cfb5e
Michael Pierce <mikee385@gmail.com>
2020-03-05 21:14:38 -0600

    Update version number

--- apps/garage-light-automation.groovy
+++ apps/garage-light-automation.groovy
@@ -14,7 +14,7 @@
  *
  */
  
-String getVersionNum() { return "1.0.0" }
+String getVersionNum() { return "1.0.1" }
 String getVersionLabel() { return "Garage Light Automation, version ${getVersionNum()} on ${getPlatform()}" }
 
 definition(
