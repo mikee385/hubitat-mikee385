6e4cd1e30749f5a4c6367fda03ac22dc35fadf25
Michael Pierce <mikee385@gmail.com>
2020-06-29 07:44:43 -0500

    Handle person waking up when bedroom door is left open

--- apps/person-with-power.groovy
+++ apps/person-with-power.groovy
@@ -31,36 +31,24 @@ preferences {
     page(name: "settings", title: "Person Automation with Power Meter", install: true, uninstall: true) {
         section {
             input "person", "device.PersonStatus", title: "Person Status", multiple: false, required: true
-            
             input "presenceSensor", "capability.presenceSensor", title: "Presence Sensor", multiple: false, required: true
-            
             input "powerMeter", "capability.powerMeter", title: "Power Meter", multiple: false, required: true
-            
             input "powerLevelForSleep", "decimal", title: "Power Level for Sleep", required: true
-            
             input "guest", "capability.presenceSensor", title: "Guest", multiple: false, required: true
-            
             input "bedroomDoor", "capability.contactSensor", title: "Bedroom Door", multiple: false, required: true
-            
             input "exteriorDoors", "capability.contactSensor", title: "Exterior Doors", multiple: true, required: true
-            
             input "backupButton", "capability.pushableButton", title: "Backup Button", multiple: false, required: false
         }
         section("Notifications") {
         
             input "alertArrived", "bool", title: "Alert when Arrived?", required: true, defaultValue: false
-            
             input "alertDeparted", "bool", title: "Alert when Departed?", required: true, defaultValue: false
-            
             input "alertAwake", "bool", title: "Alert when Awake?", required: true, defaultValue: false
-            
             input "alertAsleep", "bool", title: "Alert when Asleep?", required: true, defaultValue: false
-            
             input "notifier", "capability.notification", title: "Notification Device", multiple: false, required: true
         }
         section {
             input name: "logEnable", type: "bool", title: "Enable debug logging?", defaultValue: false
-            
             label title: "Assign a name", required: true
         }
     }
@@ -83,9 +71,9 @@ def initialize() {
     
     subscribe(powerMeter, "power", powerMeterHandler)
     
-    subscribe(bedroomDoor, "contact.open", bedroomDoorHandler)
+    subscribe(bedroomDoor, "contact", bedroomDoorHandler)
     for (door in exteriorDoors) {
-        subscribe(door, "contact.open", exteriorDoorHandler)
+        subscribe(door, "contact", exteriorDoorHandler)
     }
     
     if (backupButton) {
@@ -142,7 +130,7 @@ def bedroomDoorHandler(evt) {
     logDebug("${evt.device} changed to ${evt.value}")
     
     if (guest.currentValue("presence") == "not present") {
-        if (powerMeter.currentValue("power") < powerLevelForSleep && person.currentValue("state") == "sleep") {
+        if (powerMeter.currentValue("power") < powerLevelForSleep && person.currentValue("state") == "sleep" && state.wakingUp = false) {
             state.wakingUp = true
             if (alertAwake) {
                 notifier.deviceNotification("$person is out of bed!")
@@ -155,7 +143,7 @@ def exteriorDoorHandler(evt) {
     logDebug("${evt.device} changed to ${evt.value}")
     
     if (guest.currentValue("presence") == "not present") {
-        if (state.wakingUp == true) {
+        if (state.wakingUp == true || bedroomDoor.currentValue("contact") == "open") {
             state.wakingUp = false
             if (person.currentValue("state") == "sleep") {
                 person.awake()
