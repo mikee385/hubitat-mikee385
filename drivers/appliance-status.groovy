3fb68a466a9cd46858caeeeaca875888a3eeff5f
Michael Pierce <mikee385@gmail.com>
2022-01-24 09:31:03 -0600

    Add child buttons for each command

--- drivers/appliance-status.groovy
+++ drivers/appliance-status.groovy
@@ -1,7 +1,7 @@
 /**
  *  Appliance Status Device Handler
  *
- *  Copyright 2021 Michael Pierce
+ *  Copyright 2022 Michael Pierce
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -14,7 +14,7 @@
  *
  */
  
-String getVersionNum() { return "3.0.1" }
+String getVersionNum() { return "4.0.0" }
 String getVersionLabel() { return "Appliance Status, version ${getVersionNum()} on ${getPlatform()}" }
 
 metadata {
@@ -44,15 +44,36 @@ def installed() {
     initialize()
 }
 
+def uninstalled() {
+    for (device in getChildDevices()) {
+        deleteChildDevice(device.deviceNetworkId)
+    }
+}
+
 def updated() {
     unschedule()
     initialize()
 }
 
 def initialize() {
+    def startButton = childDevice("Start")
+    def finishButton = childDevice("Finish")
+    def resetButton = childDevice("Reset")
+
     if (!device.currentValue("status")) {
         reset()
     }
+}
+
+def childDevice(name) {
+    def childID = "appliance:${device.getId()}:$name"
+    def child = getChildDevice(childID)
+    if (!child) {
+        def childName = "${device.label ?: device.name}"
+        child = addChildDevice("mikee385", "Child Button", childID, [label: "$childName $name", isComponent: true])
+        child.setCommand(name)
+    }
+    return child
 }
 
 def on() {
@@ -79,4 +100,16 @@ def reset() {
     sendEvent(name: "status", value: "idle", descriptionText: "$device.displayName changed to idle")    
     sendEvent(name: "switch", value: "off")
     sendEvent(name: "resetTime", value: new Date())
+}
+
+def runCommand(name) {
+    if (name == "Start") {
+        start()
+    } else if (name == "Finish") {
+        finish()
+    } else if (name == "Reset") {
+        reset()
+    } else {
+        log.error "Unknown command name: $name"
+    }
 }
