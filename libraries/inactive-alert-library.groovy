a7519fd2f3953a68aaa340b7a08d759dfe89f342
Michael Pierce <mikee385@gmail.com>
2022-02-16 00:44:33 -0600

    Change timestamp of last activtiy to duration since last activity

--- libraries/inactive-alert-library.groovy
+++ libraries/inactive-alert-library.groovy
@@ -1,11 +1,11 @@
 /**
  *  name: Inactive Alert Library
  *  author: Michael Pierce
- *  version: 1.2.0
+ *  version: 1.3.0
  *  minimumHEVersion: 2.2.8
  *  licenseFile: https://raw.githubusercontent.com/mikee385/hubitat-mikee385/master/LICENSE
- *  releaseNotes: Rename methods for clarity
- *  dateReleased: 2022-02-11
+ *  releaseNotes: Change timestamp of last activtiy to duration since last activity
+ *  dateReleased: 2022-02-16
  *
  *  Copyright 2022 Michael Pierce
  *
@@ -49,7 +49,7 @@ def inactiveCheck() {
                         def cutoffTime = now() - (item.inactiveHours * 60*60*1000)
                         if (item.device.getLastActivity().getTime() <= cutoffTime) {
                             deviceIDs.add(item.device.id)
-                            personToNotify.inactiveNotification("${item.device} - ${item.device.getLastActivity().format(dateTimeFormat, location.timeZone)}")
+                            personToNotify.inactiveNotification("${item.device} - ${timeSince(item.device.getLastActivity())}")
                         }
                     } else {
                         deviceIDs.add(item.device.id)
@@ -67,7 +67,7 @@ def inactiveCheck() {
                         def cutoffTime = now() - (item.inactiveHours * 60*60*1000)
                         if (lastEvent.getDate().getTime() <= cutoffTime) {
                             deviceIDs.add(item.device.id)
-                            personToNotify.inactiveNotification("${item.device} - ${item.attribute} - ${lastEvent.getDate().format(dateTimeFormat, location.timeZone)}")
+                            personToNotify.inactiveNotification("${item.device} - ${item.attribute} - ${timeSince(lastEvent.getDate().getTime())}")
                         }
                     } else {
                         deviceIDs.add(item.device.id)
@@ -77,4 +77,30 @@ def inactiveCheck() {
             }
         }
     }
+}
+
+def timeSince(date) {
+    def seconds = Math.floor((now() - date) / 1000)
+    
+    def interval = seconds / 31536000
+    if (interval > 1) {
+        return (int)Math.floor(interval) + " years"
+    }
+    interval = seconds / 2592000
+    if (interval > 1) {
+        return (int)Math.floor(interval) + " months"
+    }
+    interval = seconds / 86400
+    if (interval > 1) {
+        return (int)Math.floor(interval) + " days"
+    }
+    interval = seconds / 3600
+    if (interval > 1) {
+        return (int)Math.floor(interval) + " hours"
+    }
+    interval = seconds / 60.
+    if (interval > 1) {
+        return (int)Math.floor(interval) + " minutes"
+    }
+    return (int)Math.floor(seconds) + " seconds"
 }
