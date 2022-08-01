00f3143834dbfe69ad1ebc179a26d48e85b52d47
Michael Pierce <mikee385@gmail.com>
2022-07-31 23:14:35 -0500

    Initial commit of Garage Door Automation

---
+++ apps/garage-door-automation.groovy
@@ -0,0 +1,173 @@
+/**
+ *  Garage Dooor Automation
+ *
+ *  Copyright 2022 Michael Pierce
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+ 
+String getVersionNum() { return "1.0.0" }
+String getVersionLabel() { return "Garage Door Automation, version ${getVersionNum()} on ${getPlatform()}" }
+
+#include mikee385.debug-library
+#include mikee385.away-alert-library
+#include mikee385.tamper-alert-library
+#include mikee385.battery-alert-library
+#include mikee385.inactive-alert-library
+
+definition(
+    name: "Garage Door Automation",
+    namespace: "mikee385",
+    author: "Michael Pierce",
+    description: "Combines several sensors and controllers into a single garage door device.",
+    category: "My Apps",
+    iconUrl: "",
+    iconX2Url: "",
+    importUrl: "https://raw.githubusercontent.com/mikee385/hubitat-mikee385/master/apps/garage-door-automation.groovy"
+)
+
+preferences {
+    page(name: "settings", title: "Garage Door Automation", install: true, uninstall: true) {
+        section {
+            input "controllers", "capability.garageDoorControl", title: "Controllers", multiple: true, required: false
+            input "sensors", "capability.contactSensor", title: "Sensors", multiple: true, required: false
+            input "alertInconsistent", "bool", title: "Alert when Sensors are Inconsistent?", required: true, defaultValue: true
+        }
+        section {
+            input "personToNotify", "device.PersonStatus", title: "Person to Notify", multiple: false, required: true
+            input name: "enableDebugLog", type: "bool", title: "Enable debug logging?", defaultValue: false
+            label title: "Assign a name", required: true
+        }
+    }
+}
+
+def installed() {
+    initialize()
+}
+
+def uninstalled() {
+    for (device in getChildDevices()) {
+        deleteChildDevice(device.deviceNetworkId)
+    }
+}
+
+def updated() {
+    unsubscribe()
+    unschedule()
+    initialize()
+}
+
+def initialize() {
+    // Child Device
+    def child = childDevice()
+    
+    // Door Contact
+    for (controller in controllers) {
+        subscribe(controller, "door", handler_DoorContact)
+    }
+    for (sensor in sensors) {
+        subscribe(sensor, "contact", handler_DoorContact)
+    }
+    
+    // Inconsistency Check
+    if (alertInconsistent) {
+        for (controller in controllers) {
+            subscribe(controller, "door", handler_InconsistencyCheck)
+        }
+        for (sensor in sensors) {
+            subscribe(sensor, "contact", handler_InconsistencyCheck)
+        }
+    }
+        
+    // Away Alert
+    for (controller in controllers) {
+        subscribe(controller, "door", handler_AwayAlert)
+    }
+    for (sensor in sensors) {
+        subscribe(sensor, "contact", handler_AwayAlert)
+    }
+    
+    // Battery Alert
+    scheduleBatteryCheck()
+    
+    // Inactive Alert
+    scheduleInactiveCheck()
+}
+
+def childDevice() {
+    def childID = "garageVirtualDoor:" + app.getId()
+    def child = getChildDevice(childID)
+    if (!child) {
+        child = addChildDevice("hubitat", "Virtual Contact Sensor", childID, 1234, [label: "Garage Virtual Door", isComponent: false])
+    }
+    return child
+}
+
+def getBatteryThresholds() {
+    var thresholds = []
+    
+    for (sensor in sensors) {
+        thresholds.add([device: sensor, lowBattery: 10])
+    }
+    
+    return thresholds
+}
+
+def getInactiveThresholds() {
+    var thresholds = []
+    
+    for (controller in controllers) {
+        thresholds.add([device: controller, inactiveHours: 24])
+    }
+    for (sensor in sensors) {
+        thresholds.add([device: sensor, inactiveHours: 24])
+    }
+    
+    return thresholds
+}
+
+def handler_DoorContact(evt) {
+    logDebug("handler_DoorContact: ${evt.device} changed to ${evt.value}")
+    
+    if (evt.value == "open") {
+        childDevice().open()
+    } else if (evt.value == "closed") {
+        childDevice().close()
+    } 
+}
+
+def handler_InconsistencyCheck(evt) {
+    logDebug("handler_InconsistencyCheck: ${evt.device} changed to ${evt.value}")
+    
+    runIn(5*60, inconsistencyCheck)
+}
+
+def inconsistencyCheck() {
+    def virtualValue = childDevice().currentValue("contact")
+    
+    for (controller in controllers) {
+        def doorValue = controller.currentValue("door")
+        if (doorValue != virtualValue) {
+            def message = "WARNING: $controller failed to change to $virtualValue!"
+            log.warn(message)
+            personToNotify.deviceNotification(message)
+        }
+    }
+    
+    for (sensor in sensors) {
+        def doorValue = sensor.currentValue("contact")
+        if (doorValue != virtualValue) {
+            def message = "WARNING: $sensor failed to change to $virtualValue!"
+            log.warn(message)
+            personToNotify.deviceNotification(message)
+        }
+    }
+}
