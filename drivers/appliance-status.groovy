140f2a056110bb99bf45c51674630a56e8cb6915
Michael Pierce <mikee385@gmail.com>
2023-01-05 06:55:41 -0600

    Remove child devices, switch capability, and time attributes

--- drivers/appliance-status.groovy
+++ drivers/appliance-status.groovy
@@ -1,7 +1,7 @@
 /**
  *  Appliance Status Device Handler
  *
- *  Copyright 2022 Michael Pierce
+ *  Copyright 2023 Michael Pierce
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -14,7 +14,7 @@
  *
  */
  
-String getVersionNum() { return "5.0.1" }
+String getVersionNum() { return "6.0.0" }
 String getVersionLabel() { return "Appliance Status, version ${getVersionNum()} on ${getPlatform()}" }
 
 metadata {
@@ -26,14 +26,9 @@ metadata {
 	) {
         capability "Actuator"
         capability "Sensor"
-        capability "Switch"
 
         attribute "status", "enum", ["idle", "running", "finished"]
         
-        attribute "startTime", "string"
-        attribute "finishTime", "string"
-        attribute "resetTime", "string"
-
         command "start"
         command "finish"
         command "reset"
@@ -44,88 +39,25 @@ def installed() {
     initialize()
 }
 
-def uninstalled() {
-    for (device in getChildDevices()) {
-        deleteChildDevice(device.deviceNetworkId)
-    }
-}
-
 def updated() {
     unschedule()
     initialize()
 }
 
 def initialize() {
-    def startButton = childDevice("Start")
-    def finishButton = childDevice("Finish")
-    def resetButton = childDevice("Reset")
-
     if (!device.currentValue("status")) {
         reset()
     }
 }
 
-def childDevice(name) {
-    def childID = "appliance:${device.getId()}:$name"
-    def child = getChildDevice(childID)
-    if (!child) {
-        def childName = "${device.label ?: device.name}"
-        child = addChildDevice("hubitat", "Generic Component Switch", childID, [label: "$childName $name", isComponent: true])
-        child.updateSetting("logEnable", [value: "false", type: "bool"])
-        child.updateSetting("txtEnable", [value: "false", type: "bool"])
-        child.updateDataValue("Name", name)
-        child.sendEvent(name: "switch", value: "off")
-    }
-    return child
-}
-
-def componentRefresh(cd) {}
-
-def componentOn(cd) {
-    def child = getChildDevice(cd.deviceNetworkId)
-    child.sendEvent(name: "switch", value: "on")
-    
-    def name = child.getDataValue("Name")
-    if (name == "Start") {
-        start()
-    } else if (name == "Finish") {
-        finish()
-    } else if (name == "Reset") {
-        reset()
-    } else {
-        log.error "Unknown command name: $name"
-    }
-    
-    runIn(1, componentOff, [data: [deviceNetworkId: cd.deviceNetworkId]])
-}
-
-def componentOff(cd) {
-    def child = getChildDevice(cd.deviceNetworkId)
-    child.sendEvent(name: "switch", value: "off")
-}
-
-def on() {
-    start()
-}
-
-def off() {
-    finish()
-}
-
 def start() {
     sendEvent(name: "status", value: "running", descriptionText: "$device.displayName changed to running")    
-    sendEvent(name: "switch", value: "on")
-    sendEvent(name: "startTime", value: new Date())
 }
 
 def finish() {
     sendEvent(name: "status", value: "finished", descriptionText: "$device.displayName changed to finished")    
-    sendEvent(name: "switch", value: "off")
-    sendEvent(name: "finishTime", value: new Date())
 }
 
 def reset() {
     sendEvent(name: "status", value: "idle", descriptionText: "$device.displayName changed to idle")    
-    sendEvent(name: "switch", value: "off")
-    sendEvent(name: "resetTime", value: new Date())
 }
