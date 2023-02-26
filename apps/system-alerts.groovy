1c9a94244ca9ef53f2ba2dc3d3734e57c49f97db
Michael Pierce <mikee385@gmail.com>
2023-02-25 21:41:53 -0600

    Add alert for Firmware Update Available

--- apps/system-alerts.groovy
+++ apps/system-alerts.groovy
@@ -1,7 +1,7 @@
 /**
  *  System Alerts
  *
- *  Copyright 2022 Michael Pierce
+ *  Copyright 2023 Michael Pierce
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -14,7 +14,7 @@
  *
  */
  
-String getVersionNum() { return "3.2.0" }
+String getVersionNum() { return "3.3.0" }
 String getVersionLabel() { return "System Alerts, version ${getVersionNum()} on ${getPlatform()}" }
 
 #include mikee385.debug-library
@@ -39,6 +39,7 @@ preferences {
             input "alertZigbeeOff", "bool", title: "Alert when Zigbee Off?", required: true, defaultValue: true
             input "alertZigbeeOn", "bool", title: "Alert when Zigbee On?", required: true, defaultValue: true
             input "alertZwaveCrashed", "bool", title: "Alert when Z-Wave Crashed?", required: true, defaultValue: true
+            input "alertFirmwareUpdate", "bool", title: "Alert when Firmware Update Available?", required: true, defaultValue: true
         }
         section {
             input "personToNotify", "device.PersonStatus", title: "Person to Notify", multiple: false, required: true
@@ -82,6 +83,14 @@ def initialize() {
     if (alertZwaveCrashed) {
         subscribe(location, "zwaveCrashed", zwaveCrashedHandler)
     }
+    
+    if (alertFirmwareUpdate) {
+        def currentTime = new Date()
+        def runTime = timeToday("20:00")
+    
+        schedule("$currentTime.seconds $runTime.minutes $runTime.hours * * ? *", checkForUpdate)
+        checkForUpdate()
+    }
 }
 
 def rebootHandler(evt) {
@@ -118,4 +127,30 @@ def zwaveCrashedHandler(evt) {
     logDebug("zwaveCrashedHandler: ${evt.device} changed to ${evt.value}")
     
     personToNotify.deviceNotification("Z-Wave has crashed!")
+}
+
+def checkForUpdate() {
+	def params = [
+        uri: "http://127.0.0.1:8080",
+        path:"/hub/cloud/checkForUpdate",
+		contentType: "application/json"
+	]
+	def result = null
+	logDebug("Checking for firmware update...")
+	try {
+		httpGet(params) { resp ->
+			logDebug("Status: ${resp.status}, Data: ${resp.data}")
+			if (resp.status == 200) {
+            	if(resp.data.status != "NO_UPDATE_AVAILABLE") {
+	            	logDebug("Firmware update available: ${resp.data.version}")
+			        personToNotify.deviceNotification("Firmware update available: ${resp.data.version}")
+	            } else {
+	                logDebug("Firmware is up to date")
+			    }
+			}
+		}
+	}
+	catch (e) {
+		log.error("Exception Received: $e")
+	}
 }
